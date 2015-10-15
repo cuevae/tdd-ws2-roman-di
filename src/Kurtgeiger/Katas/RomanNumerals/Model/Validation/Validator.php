206ec85d84c1b4b8123a464fd3d3544bc5c96ba5
Enmanuel Cueva <enmanuel.cueva@kurtgeiger.com>
2015-10-15 16:12:54 +0100

    first commit

---
+++ src/Kurtgeiger/Katas/RomanNumerals/Model/Validation/Validator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Kurt Geiger Validator
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category
+ * @package
+ * @copyright  Copyright (c) 2015 Kurt Geiger (http://www.kurtgeiger.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Kurtgeiger\Katas\RomanNumerals\Model\Validation;
+
+
+class Validator
+{
+
+}
---
+++ src/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Kurt Geiger Katas RomanNumerals Bootstrap
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category
+ * @package
+ * @copyright  Copyright (c) 2015 Kurt Geiger (http://www.kurtgeiger.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+define('PROJECT_ROOT', dirname(dirname(__FILE__)));
+define('DS', DIRECTORY_SEPARATOR);
+require_once PROJECT_ROOT . '/vendor/autoload.php';
